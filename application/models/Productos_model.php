5c74d0dbf7bf46758865dafd7fa774deb9934a7d
jrgonzalez3 <jrgonzalez3@gmail.com>
2021-06-18 13:22:23 -0400

    desarrollo - ok modulo de productos

--- application/models/Productos_model.php
+++ application/models/Productos_model.php
@@ -1,36 +1,50 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Productos_model extends CI_Model {
-	//estos son metodos q tienen q ver con bd
-	
-	//este metodo es para mostrar todos los registros de la tabla
-	public function getProductos(){
-		$this->db->select("p.*, s.descripcion"); //selecc campos
-		$this->db->from("producto p"); //desde tabla con alias
-		$this->db->join("servicio s", "s.id_serv=p.id_serv");//une los campos por su pk=fk
-		//$this->db->where("p.estado", "1");
-		$resultados= $this->db->get();
-		return $resultados->result();
-	}
-	
-	//esto es una funcion o metodo para mostrar 1 registro de la tabla x su id
-	public function getProducto($id){
-		$this->db->where("id_prod",$id);
-		$resultado= $this->db->get("producto");
-		return $resultado->row();
-	}
-	
-	//esta es la parte para guardar en la bd
-	public function save($data)
-	{
-		return $this->db->insert("producto", $data);
-	}
-	
-	
-	//esto es para actualizar los datos de la tabla
-	public function update($id, $data){
-		$this->db->where("id_prod", $id);
-		return $this->db->update("producto", $data);
-	}
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Productos_model extends CI_Model
+{
+    public $tabla = 'producto_servicio';
+    public $idTabla = 'id_producto';
+
+    //este metodo es para mostrar todos los registros de la tabla
+    public function getAll()
+    {
+        $this->db->select('p.*, c.descripcion as categoria');
+        $this->db->from('producto_servicio p'); //desde tabla con alias
+        $this->db->join('categoria c', 'c.id_categoria=p.id_categoria'); //une los campos por su pk=fk
+        $this->db->where('p.tipo', '1'); //1 es producto tipo 2 es servicio
+        $this->db->where('p.estado', '1'); //1 es activo 2 inactivo 3 anulado
+        $result = $this->db->get();
+
+        return $result->result();
+    }
+
+    //esto es una funcion o metodo para mostrar 1 registro de la tabla x su id
+    public function getById($id)
+    {
+        $this->db->select('p.*, c.descripcion as categoria');
+        $this->db->from('producto_servicio p'); //desde tabla con alias
+        $this->db->join('categoria c', 'c.id_categoria=p.id_categoria'); //une los campos por su pk=fk
+        $this->db->where('p.tipo', '1'); //1 es producto tipo 2 es servicio
+        $this->db->where('p.estado', '1'); //1 es activo 2 inactivo 3 anulado
+        $this->db->where('p.id_producto', $id); //1 es activo 2 inactivo 3 anulado
+        $result = $this->db->get();
+
+        return $result->row();
+    }
+
+    //esta es la parte para guardar en la bd
+    public function save($data)
+    {
+        return $this->db->insert($this->tabla, $data);
+    }
+
+    //esto es para actualizar los datos de la tabla
+    public function update($id, $data)
+    {
+        $this->db->where($this->idTabla, $id);
+
+        return $this->db->update($this->tabla, $data);
+    }
 }
