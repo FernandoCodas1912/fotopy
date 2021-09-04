fcb812f5ba97dc82fd1ba316a0eacc7a5f170cec
jrgonzalez3 <jrgonzalez3@gmail.com>
2021-09-04 10:13:22 -0400

    SE adapta ventas, boleta y condicion venta en las vistas

--- application/views/ventas/list.php
+++ application/views/ventas/list.php
@@ -2,120 +2,129 @@
 <!-- Start right Content here -->
 <!-- ============================================================== -->
 <div class="content-page">
-	<!-- Start content -->
-	<div class="content">
-		
-		<?php
-		if($this->session->flashdata("success")): ?>
-			<div class="alert alert-success" role="alert">
-				<button type="button" class="close" data-dismiss="alert">
-					&times;
-				</button>
-				<strong>
-					¡Buen Trabajo!
-				</strong>
-				<p>
-					<?php echo $this->session->flashdata("success")?>
-				</p>
-			</div>
-			
-		<?php endif; ?>
-		<?php
-		if($this->session->flashdata("error")): ?>
-			<div class="alert alert-danger" role="alert">
-				<button type="button" class="close" data-dismiss="alert">
-					&times;
-				</button>
-				<strong>
-					¡Error!
-				</strong>
-				<p>
-					<?php echo $this->session->flashdata("error")?>
-				</p>
-			</div>
-		<?php endif; ?>
-		<!-- end row -->
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="card-box">
-					<div class="col-md-2 pull-right">
-						<a href="<?php echo base_url();?>Ventas_controller/add" class="btn btn-success"><i class="fa fa-plus"></i> <span>Nueva Venta</span> </a>
-					</div>
-					<h4 class="header-title m-t-0 m-b-30"> Listado de Ventas </h4>
-					<div class="table-responsive">
-						<table class="table" id="example3">
-							<thead>
-								<tr>
-									<th>Op</th>
-									<th>Fecha</th>
-									<th>Cliente</th>
-									<th>Total</th>
-									<th>F.Pago</th>
-									<th>T.Comprob.</th>
-									<th>Estado</th>
-									<th>Acciones</th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php
-								if(!empty($ventas)):?>
-									<?php foreach($ventas as $venta):?>
-										<tr>
-											<td><?php echo $venta->id_venta;?></td>
-											<?php 
+    <!-- Start content -->
+    <div class="content">
+
+        <?php
+		if ($this->session->flashdata("success")) : ?>
+        <div class="alert alert-success" role="alert">
+            <button type="button" class="close" data-dismiss="alert">
+                &times;
+            </button>
+            <strong>
+                ¡Buen Trabajo!
+            </strong>
+            <p>
+                <?php echo $this->session->flashdata("success") ?>
+            </p>
+        </div>
+
+        <?php endif; ?>
+        <?php
+		if ($this->session->flashdata("error")) : ?>
+        <div class="alert alert-danger" role="alert">
+            <button type="button" class="close" data-dismiss="alert">
+                &times;
+            </button>
+            <strong>
+                ¡Error!
+            </strong>
+            <p>
+                <?php echo $this->session->flashdata("error") ?>
+            </p>
+        </div>
+        <?php endif; ?>
+        <!-- end row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card-box">
+                    <div class="col-md-2 pull-right">
+                        <a href="<?php echo base_url(); ?>Ventas_controller/add" class="btn btn-success"><i
+                                class="fa fa-plus"></i> <span>Nueva Venta</span> </a>
+                    </div>
+                    <h4 class="header-title m-t-0 m-b-30"> Listado de Ventas </h4>
+                    <div class="table-responsive">
+                        <table class="table" id="example3">
+                            <thead>
+                                <tr>
+                                    <th>Op</th>
+                                    <th>Fecha</th>
+                                    <th>Cliente</th>
+                                    <th>Total</th>
+                                    <th>F.Pago</th>
+                                    <th>T.Comprob.</th>
+                                    <th>Cond. Venta</th>
+                                    <th>Estado</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+								if (!empty($ventas)) : ?>
+                                <?php foreach ($ventas as $venta) : ?>
+                                <tr>
+                                    <td><?php echo $venta->id_venta; ?></td>
+                                    <?php
 											$fecha = $venta->fecha;
 											$fecha = date('d-m-Y', strtotime($fecha));
 											$estado = $venta->estado;
 
-											if($estado == 1)
-											{
-												$estado2     = "Activo";$label_class = 'label-success';
-											}
-											else
-											{
-												if($estado == 2)
-												{
-													$estado2     = "Inactivo";$label_class = 'label-warning';
-												}
-												else
-												{
-													$estado2     = "Anulado";$label_class = 'label-danger';
+											if ($estado == 1) {
+												$estado2     = "Activo";
+												$label_class = 'label-success';
+											} else {
+												if ($estado == 2) {
+													$estado2     = "Inactivo";
+													$label_class = 'label-warning';
+												} else {
+													$estado2     = "Anulado";
+													$label_class = 'label-danger';
 												}
 											}
-											$tipo=	$venta->tipocomprobante;
-											if($tipo == 1)
-											{
-												$tipo2     = "Factura";$label_class2 = 'label-success';
-
+											$tipo =	$venta->tipocomprobante;
+											if ($tipo == 1) {
+												$tipo2     = "Factura";
+												$label_class2 = 'label-success';
+											} else {
+												$tipo2     = "Boleta";
+												$label_class2 = 'label-warning';
 											}
-											else
-											{
-												$tipo2     = "Boleta";$label_class2 = 'label-warning';
+											$condicion = $venta->condicionventa;
+											if ($condicion == 1) {
+												$condicion2     = "Contado";
+												$label_condicion = 'label-primary';
+											} else {
+												$condicion2     = "Crédito";
+												$label_condicion = 'label-secondary';
 											}
 											?>
-											<td><?php echo $fecha;?></td>
-											<td><?php echo $venta->id_cliente." - ".$venta->nombre;?></td>
-											<td><?php echo $venta->total;?></td>
-											<td><?php echo $venta->formadepago;?></td>
-											<td><?php echo $tipo2;?></td>
-											<td><span class="label <?php echo $label_class;?>"><?php echo $estado2; ?></span></td>
-											<td>
-												<a href="<?php echo base_url();?>Ventas_controller/view/<?php echo $venta->id_venta;?>" class="btn btn-sm btn-primary" title="Ver y editar detalles de la venta">
-													<i class="fa fa-eye">
-													</i> Ver mas detalles
-												</a>
+                                    <td><?php echo $fecha; ?></td>
+                                    <td><?php echo $venta->id_cliente . " - " . $venta->nombre; ?></td>
+                                    <td><?php echo $venta->total; ?></td>
+                                    <td><?php echo $venta->formadepago; ?></td>
+                                    <td><?php echo $tipo2; ?></td>
+                                    <td><span
+                                            class="label <?php echo $label_condicion; ?>"><?php echo $condicion2; ?></span>
+                                    <td><span class="label <?php echo $label_class; ?>"><?php echo $estado2; ?></span>
+                                    </td>
+                                    <td>
+                                        <a href="<?php echo base_url(); ?>Ventas_controller/view/<?php echo $venta->id_venta; ?>"
+                                            class="btn btn-sm btn-primary" title="Ver y editar detalles de la venta">
+                                            <i class="fa fa-eye">
+                                            </i> Ver mas detalles
+                                        </a>
 
-												
-											</td>
-										</tr>
 
-									<?php endforeach; ?>
-								<?php endif; ?>	
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div><!-- end col -->
-		</div>
-		<!-- end row -->
-</div> <!-- container -->
+                                    </td>
+                                </tr>
+
+                                <?php endforeach; ?>
+                                <?php endif; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div><!-- end col -->
+        </div>
+        <!-- end row -->
+    </div> <!-- container -->
--- application/views/ventas/view.php
+++ application/views/ventas/view.php
@@ -7,7 +7,7 @@
         <div class="container">
 
             <?php
-			if ($this->session->flashdata("success")) : ?>
+            if ($this->session->flashdata("success")) : ?>
             <div class="alert alert-success" role="alert">
                 <button type="button" class="close" data-dismiss="alert">
                     &times;
@@ -22,7 +22,7 @@
 
             <?php endif; ?>
             <?php
-			if ($this->session->flashdata("error")) : ?>
+            if ($this->session->flashdata("error")) : ?>
             <div class="alert alert-danger" role="alert">
                 <button type="button" class="close" data-dismiss="alert">
                     &times;
@@ -55,24 +55,34 @@
                         </div>
                         <div class="col-xs-4">
                             <?php
-							//formateamos el tipo de comprobante
-							$tipo = $venta->tipocomprobante;
-							if ($tipo == 1) {
-								$tipo2     = "Factura";
-								$label_class2 = 'label-success';
-							} else {
-								$tipo2     = "Boleta";
-								$label_class2 = 'label-warning';
-							}
-							?>
+                            //formateamos el tipo de comprobante
+                            $tipo = $venta->tipocomprobante;
+                            if ($tipo == 1) {
+                                $tipo2     = "Factura";
+                                $label_class = 'label-success';
+                            } else {
+                                $tipo2     = "Boleta";
+                                $label_class = 'label-warning';
+                            }
+                            $condicion = $venta->condicionventa;
+                            if ($condicion == 1) {
+                                $condicion2     = "Contado";
+                                $label_class2 = 'label-primary';
+                            } else {
+                                $condicion2     = "Crédito";
+                                $label_class2 = 'label-secondary';
+                            }
 
+                            ?>
 
-                            <b> OPERACION </b><br>
-                            <b>ID: </b><?php echo $venta->id_venta; ?><br>
-                            <b>Comprobante: </b><?php echo $tipo2; ?><br>
+                            <b> OPERACION #</b><?php echo $venta->id_venta; ?><br>
+                            <b>Comprobante: </b><span
+                                class="label <?php echo $label_class; ?>"><?php echo $tipo2; ?></span><br>
                             <b>Serie: </b><?php echo $venta->seriecomprobante; ?><br>
                             <b>Nro: </b><?php echo $venta->nrocomprobante; ?><br>
                             <b>Fecha Op: </b><?php echo date('d-m-Y', strtotime($venta->fecha)); ?><br>
+                            <b>Condición de Venta : </b><span
+                                class="label <?php echo $label_class2; ?>"><?php echo $condicion2; ?></span><br>
 
                         </div>
 
@@ -87,12 +97,14 @@
                             data-target="#modal-print" value="<?php echo $venta->id_venta; ?>"
                             title="Generar comprobante"><i class="fa fa-print"></i> Vista Impresion
                         </button>
+                        <?php if ($venta->estado != 3) { ?>
 
                         <a href="<?php echo base_url(); ?>Ventas_controller/delete/<?php echo $venta->id_venta; ?>"
                             <button type="button" class="btn btn-danger btn-sm btn-delete"
                             value="<?php echo $venta->id_venta; ?>" title="Anular"><i class="fa fa-trash-o"></i> Anular
                             </button>
                         </a>
+                        <?php } ?>
 
                         <a href="<?php echo base_url(); ?>Ventas_controller" <button type="button"
                             class="btn btn-primary btn-sm" title="Regresar al listado"><i class="fa fa-arrow-left"></i>
@@ -117,23 +129,23 @@
                                 </thead>
                                 <tbody>
                                     <?php
-									if (!empty($detalles)) :
-										$sumadorIva10 = 0;
-										$sumadorIva5 = 0;
-										$total_iva10 = 0;
-										$total_iva5 = 0;
-										$suma_ivas = 0;
-										foreach ($detalles as $detalle) :
+                                    if (!empty($detalles)) :
+                                        $sumadorIva10 = 0;
+                                        $sumadorIva5 = 0;
+                                        $total_iva10 = 0;
+                                        $total_iva5 = 0;
+                                        $suma_ivas = 0;
+                                        foreach ($detalles as $detalle) :
 
-											$impuesto = $detalle->impuesto;
-											if ($impuesto == '10') {
-												$calculariva10 = $detalle->importe / 11;
-												$sumadorIva10 = $sumadorIva10 + $calculariva10;
-											} elseif ($impuesto == '5') {
-												$calculariva5 = $detalle->importe / 21;
-												$sumadorIva5 += $calculariva5;
-											}
-									?>
+                                            $impuesto = $detalle->impuesto;
+                                            if ($impuesto == '10') {
+                                                $calculariva10 = $detalle->importe / 11;
+                                                $sumadorIva10 = $sumadorIva10 + $calculariva10;
+                                            } elseif ($impuesto == '5') {
+                                                $calculariva5 = $detalle->importe / 21;
+                                                $sumadorIva5 += $calculariva5;
+                                            }
+                                    ?>
                                     <tr class="text-center">
                                         <td><?php echo $detalle->codigobarra; ?></td>
                                         <td align="center">
@@ -143,29 +155,29 @@
                                         <td><?php echo number_format(($detalle->precio), 0, ',', '.'); ?>
                                         </td>
                                         <td><?php if ($impuesto == '0') {
-														echo number_format(($detalle->importe), 0, ',', '.');
-													} else {
-														echo '0';
-													} ?></td>
+                                                        echo number_format(($detalle->importe), 0, ',', '.');
+                                                    } else {
+                                                        echo '0';
+                                                    } ?></td>
                                         <td><?php if ($impuesto == '5') {
-														echo number_format(($detalle->importe), 0, ',', '.');
-													} else {
-														echo '0';
-													} ?></td>
+                                                        echo number_format(($detalle->importe), 0, ',', '.');
+                                                    } else {
+                                                        echo '0';
+                                                    } ?></td>
                                         <td><?php if ($impuesto == '10') {
-														echo number_format(($detalle->importe), 0, ',', '.');
-													} else {
-														echo '0';
-													} ?></td>
+                                                        echo number_format(($detalle->importe), 0, ',', '.');
+                                                    } else {
+                                                        echo '0';
+                                                    } ?></td>
                                     </tr>
                                     <?php endforeach; ?>
                                     <?php endif; ?>
                                 </tbody>
                                 <?php
-								$total_iva10 += $sumadorIva10;
-								$total_iva5 += $sumadorIva5;
-								$suma_ivas = $total_iva5 + $total_iva10;
-								?>
+                                $total_iva10 += $sumadorIva10;
+                                $total_iva5 += $sumadorIva5;
+                                $suma_ivas = $total_iva5 + $total_iva10;
+                                ?>
                                 <tfoot>
                                     <tr>
                                         <td colspan="6" class="text-right"><strong>TOTAL: </strong></td>
